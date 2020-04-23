6492a323d4e6939f4f0f2019af71dd4b868a2e73
Casey McQuillan <Casey@quilltechnologies.com>
2020-04-22 23:10:55 -0700

    Adding new "make your own" tool

--- src/Fiddle.Run.Client/src/app/app.module.ts
+++ src/Fiddle.Run.Client/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransformBoxComponent } from './components/transform-box/transform-box.component';
 import { PalettePageComponent } from './pages/palette-page/palette-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CodeDisplayComponent } from './components/code-display/code-display.component';
 import { TestPageComponent } from './pages/test-page/test-page.component';
 import { TransformApplierDirective } from './directives/transform-applier.directive';
@@ -35,6 +38,7 @@ import { RouteTemplatePageComponent } from './pages/route-template-page/route-te
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
@@ -42,6 +46,9 @@ import { RouteTemplatePageComponent } from './pages/route-template-page/route-te
     MatSelectModule,
     MatButtonModule,
     MatTabsModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/Fiddle.Run.Client/src/app/pages/palette-page/palette-page.component.ts
+++ src/Fiddle.Run.Client/src/app/pages/palette-page/palette-page.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { TransformStoreService } from 'src/app/services/transform-store.service';
+import { FormControl } from '@angular/forms';
+import { startWith, map } from 'rxjs/operators';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'fiddle-palette-page',
   templateUrl: './palette-page.component.html',
-  styleUrls: ['./palette-page.component.scss']
+  styleUrls: ['./palette-page.component.scss'],
 })
 export class PalettePageComponent implements OnInit {
+  transformCtrl = new FormControl();
+  filteredTransforms$: Observable<string[]>;
+  @ViewChild('trInput') trInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
-  constructor() { }
+  private readonly _transforms = new BehaviorSubject<string[]>(['textInput']);
+  transforms$ = this._transforms.asObservable();
+
+  constructor(private readonly _store: TransformStoreService) {
+    this.filteredTransforms$ = this.transformCtrl.valueChanges.pipe(
+      startWith(null),
+      map((transform: string | null) => this._filter(transform))
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  addTransform(transform: string) {
+    const val = this._transforms.value;
+    this._transforms.next([...val, transform]);
+  }
+
+  selected(event: MatAutocompleteSelectedEvent) {
+    const val = this._transforms.value;
+    this._transforms.next([...val, event.option.value]);
+    this.trInput.nativeElement.value = '';
+    this.transformCtrl.setValue(null);
+  }
+
+  remove(index: number) {
+    const newArray = this._transforms.value.filter((v, i) => i !== index);
+    this._transforms.next(newArray);
+  }
+
+
+  private _filter(val: string | null) {
+    const filterVal = val ? val.toLowerCase() : '';
+    return filterVal ? this._store.list().filter(v => v.toLowerCase().indexOf(filterVal) === 0) : [];
+  }
+
+  getList(): string[] {
+    return this._store.list();
   }
 
 }
